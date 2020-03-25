3dc706c986634224886f471d2c8bd075a6d0f3be
szpakusik <szpakusik@gmail.com>
2020-03-25 13:11:33 +0100

    added new sample products

--- src/reducers/someReducer.js
+++ src/reducers/someReducer.js
@@ -3,13 +3,18 @@ const TEST_ACTION = 'TEST_ACTION';
 const initState = {
 
   products: [
-    { name:"Mleko Łęckie", weight: '1000ml', price: 4.49, photo:"mleko.jpg" },
-    { name:"Czekolada Wedel Truskawka", weight: '100g', price: 3.79, photo:"czekolada.png" }
+    { name:"Mleko Łęckie", weight: '1000ml', price: 4.49, photo:"mleko.jpg", category:"dairy" },
+    { name:"Czekolada Wedel Truskawka", weight: '100g', price: 3.79, photo:"czekolada.png", category:"dairy" },
+    { name:"Chleb Firmowy", weight: '500g', price: 2.50, photo:"bread.png", category:"grainProducts" },
+    { name:"Bułka Ziarnista", weight: '80g', price: 1.10, photo:"grainyRoll.png", category:"grainProducts" },
+    { name:"Lakier Taft 5", weight: '250ml', price: 12.30, photo:"taft.png", category:"chemistry" },
+    { name:"Antyperspirant Addidas", weight: '150ml', price: 8.99, photo:"antyper.png", category:"chemistry" },
+    { name:"Jabłka", weight: '1000g', price: 5.49, photo:"apples.png", category:"natural" },
+    { name:"Ogórki", weight: '1000g', price: 8.49, photo:"cucumbers.png", category:"natural" },
   ],
 
   cart:{
     products:[{ name:"Mleko Łęckie", weight: '1000ml', price: 4.49, photo:"mleko.jpg" },],
-
   }
 
 }
