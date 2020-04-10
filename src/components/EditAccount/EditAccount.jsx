dd23e9511082617d0471d6aa65c6bcf3debd2da5
szpakusik <szpakusik@gmail.com>
2020-04-10 20:39:00 +0200

    Edit account component

--- src/components/EditAccount/EditAccount.jsx
+++ src/components/EditAccount/EditAccount.jsx
@@ -6,9 +6,34 @@ const EditAccount = (props) => {
     return(
         <>
         
-            <div className="container w-100 text-center bg-success p-4">
-                <p className="h1 text-light">EDIT ACCOUNT</p>
+        <div className={`row my-5 p-5 pl-0 ${editAccount.editAccount}`}>
+
+            <div className="row w-100 mb-5">
+
+                <div className="text-left h1 my-3 text-dark col-md-12">
+                    Zarejestruj się już teraz... <br /> i zgarnij <span className="text-success">2 darmowe dostawy</span>!
+                </div>
+
             </div>
+
+            <div className="row w-100">
+
+                <div className={`col-md-5 ${editAccount.introduce}`}>
+                    <div className="w-100 h-100 text-light pt-4">
+                        <p className="h3">Jesteśmy</p>
+                        <img src={require('./../../images/z-dowozem-text.png')} alt=""/>
+                        <p className="text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam possimus explicabo illum asperiores dolore neque nam sequi inventore ex delectus mollitia ea reiciendis voluptatibus iure, ut debitis quibusdam consectetur quia?</p>
+                        <p className="h3">A Ty?</p>
+                    </div>
+                </div>
+
+                <div className="col-md-7">
+                    <p className="h1">Przedstaw się!</p>
+                </div>
+
+            </div>
+
+        </div>
 
         </>
     )
--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -3,6 +3,7 @@ import homeContent from './homeContent.module.scss';
 import StepCard from './stepCard/StepCard'
 import { connect } from 'react-redux';
 import { cardData } from '../../utils/content/cardData'
+import EditAccount from '../EditAccount/EditAccount';
 
 
 const HomeContent = (props) => {
@@ -13,7 +14,7 @@ const HomeContent = (props) => {
             <div className={`container w-100 text-center p-4 mb-5 ${homeContent.homeContent}`}>
                 <p className="h5 mb-4">Mieszkasz w gminie Morawica?</p>
                 <p className="h1 text-success m-0 mt-4 text-uppercase">Dostarczymy Twoje zakupy pod same drzwi!</p>
-                <img src={require('./../../images/logo-z-dowozem.png')} alt="" className="mt-3"/>
+                <img src={require('./../../images/logo-z-dowozem2.png')} alt="" className="mt-3"/>
                 <p className="h5 my-4">w 3 prostych krokach:</p>
 
                 <div className="row mt-5 text-center">
@@ -24,13 +25,7 @@ const HomeContent = (props) => {
 
                 </div>
 
-                <div className={`row my-5 p-5 pl-0 ${homeContent.registerSection}`}>
-
-                    <div className="text-left h1 my-3 text-dark">
-                        Zarejestruj się już teraz... <br /> i zgarnij <span className="text-success">2 darmowe dostawy</span>!
-                    </div>
-
-                </div>
+                <EditAccount />
 
             </div>
             
