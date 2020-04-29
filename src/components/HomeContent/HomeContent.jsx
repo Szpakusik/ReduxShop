f5f23904264e57b59b147a85eede025dce1aedd0
Szpakusik <szpakusik@gmail.com>
2020-04-29 16:15:24 +0200

    Homepage + products

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -8,12 +8,16 @@ import CreateAccount from '../CreateAccount/CreateAccount';
 
 const HomeContent = (props) => {
 
+    // const handleMenuClick = () => {
+    //     props.showSidebar( true );
+    // }
+
     return(
         <>
         
             <div className={`container w-100 text-center p-4 mb-5 ${homeContent.homeContent}`}>
                 <p className="h5 mb-4">Mieszkasz w gminie Morawica?</p>
-                <p className="h1 text-success m-0 mt-4 text-uppercase">Dostarczymy Twoje zakupy pod same drzwi!</p>
+                <p className="h2 text-success m-0 mt-4 text-uppercase">Dostarczymy Twoje zakupy pod same drzwi!</p>
                 <img src={require('./../../images/logo-z-dowozem2.png')} alt="" className="mt-3"/>
                 <p className="h5 my-4">w 3 prostych krokach:</p>
 
@@ -28,16 +32,25 @@ const HomeContent = (props) => {
                 <CreateAccount />
 
                 
-                <div className="row w-100 mb-5">
+                <div className="row w-100 mb-5 mx-auto">
 
-                    <div className={`col-md-3 ${homeContent.icon}`}>
-                        <span class="material-icons">
+                    <div className={`col-md-3 col-12 ${homeContent.icon}`}>
+                        <span className="material-icons">
                             reply
                         </span>
                     </div>
 
-                    <div className="text-right h1 my-3 text-dark col-md-9">
-                        Albo wybierz kategorię... <br /> i zacznij  <span className="text-success">bez zakładania konta</span>!
+                    <div className="text-center text-md-right h2 my-3 text-dark col-12 col-md-9">
+                        Albo wybierz kategorię... <br />
+                        
+                        <button className={`btn btn-outline-success my-3 bg-wood d-md-none mx-auto d-block`}>
+                            <span className="material-icons pt-1">
+                                menu
+                            </span>
+                        </button>
+
+                        i zacznij  
+                        <span className="text-success"> bez zakładania konta</span>!
                     </div>
 
                 </div>
@@ -57,4 +70,12 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(HomeContent)
+const mapDispatchToProps = ( dispatch ) => {
+    return{
+        showSidebar: ( status )=>{
+            dispatch( { type:"SHOW_SIDEBAR", status: status } )
+        },
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContent)
