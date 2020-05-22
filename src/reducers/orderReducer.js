77d1d4bb9ae5701ab88602c5f97d017a33e0a198
Szpakusik <szpakusik@gmail.com>
2020-05-22 08:48:33 +0200

    GET user order history

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -2,7 +2,37 @@ const initState = {
   contactDetails: {
       email: "",
       phone: "",
-  }
+  },
+  orders: [
+    // {
+    //     id: 1231,
+    //     date: new Date(),
+    //     status: 'W trakcie',
+    //     products: [
+    //         { name:"Szynka Sołtysowa", weight: '1000g', price: 25.49, photo:"ham.png", category:"mieso", id:22, amount:1 },
+    //         { name:"Kurczak Cały", weight: '1000g', price: 16.49, photo:"chicken.png", category:"mieso", id:23, amount:1 },
+    //     ],
+    // },
+    // {
+    //     id: 1231,
+    //     date: new Date(),
+    //     status: 'Nieopłacone',
+    //     products: [
+    //         { name:"Szynka Sołtysowa", weight: '1000g', price: 25.49, photo:"ham.png", category:"mieso", id:22, amount:1 },
+    //         { name:"Kurczak Cały", weight: '1000g', price: 16.49, photo:"chicken.png", category:"mieso", id:23, amount:1 },
+    //     ],
+    // },
+    // {
+    //     id: 4231,
+    //     date: new Date(),
+    //     status: 'Dostarczone',
+    //     products: [
+    //         { name:"Szynka Sołtysowa", weight: '1000g', price: 25.49, photo:"ham.png", category:"mieso", id:22, amount:1 },
+    //         { name:"Kurczak Cały", weight: '1000g', price: 16.49, photo:"chicken.png", category:"mieso", id:23, amount:1 },
+    //     ],
+    // },
+
+]
 }
 
 const orderReducer = (state = initState, action) => {
@@ -16,6 +46,13 @@ const orderReducer = (state = initState, action) => {
              phone: action.phone,
          },
       }
+    case "SET_ORDERS":
+      return{
+        ...state,
+        orders: action.orders,
+      }
+    
+
     
     default:
       return state;
